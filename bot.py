07039aac248d852313900c53036597706ec2ac07
Yann-Fournier <y.fourni@icloud.com>
2023-11-10 14:58:13 +0100

    commandes

--- bot.py
+++ bot.py
@@ -2,21 +2,15 @@ import discord
 import os
 from dotenv import load_dotenv , find_dotenv
 from discord.ext import commands
-
-intents = discord.Intents.all()
+# from django.utils.translation import gettext
 
 # Récuper le .env et stocker le token dans une variable
 load_dotenv(find_dotenv())
 bot_token = os.getenv('BOT_TOKEN')
 
-client = commands.Bot(command_prefix="!", intents = intents)
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="$", intents = intents)
 
-@client.command(name="Hello")
-async def delete(ctx):
-    messages = await ctx.channel.history(limit=10)
-
-    for each_message in messages:
-        await each_message.delete()
 
 @client.event
 async def on_ready():
@@ -31,9 +25,29 @@ async def on_member_join(member):
     general_channel = client.get_channel(1044900412551073832)
     await general_channel.send("Bienvenue sur le serveur ! "+ member.name)
 
+
+@client.command(name="hello")
+async def hello(message):
+    await message.channel.send("hello")
+
+@client.command(name="test")
+async def test(message, *args):
+  print(type(*args))
+  await message.channel.send(*args)
+  await message.channel.send(message)
+  await message.channel.send("test")
+
+
+@client.command(name="plot")
+async def plot(message, crypto, indicateur):
+  string = f"Voici un graphique de {crypto} avec le {indicateur}."
+  await message.channel.send(string)
+  
+  
 @client.event
 async def on_message(message):
   # Permet au bo de ne pas ce parler à lui même
+  # print(message.content)
   if message.author == client.user:
     return
 
