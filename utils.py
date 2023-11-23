402db6188be0bec132c0963071c1706694c4e4eb
Yann <truc@truc.truc>
2023-11-23 11:15:39 +0100

    fin graphique

--- utils.py
+++ utils.py
@@ -35,14 +35,15 @@ def get_indicator(df, ind):
     match ind:
         case "rsi":
             df["rsi"] = ta.momentum.rsi(df['close'], 14)
+            df['rsima'] = df['rsi'].rolling(14).mean()
         case "macd":
             df["macd"] = ta.trend.macd(close=df["close"], window_slow=26, window_fast=12)
             df["macd_diff"] = ta.trend.macd_diff(close=df["close"], window_slow=26, window_fast=12)
             df["macd_signal"] = ta.trend.macd_signal(close=df["close"], window_slow=26, window_fast=12, window_sign=9, fillna=False)
         case "stoch":
             df["stoch_%K"] = ta.momentum.stoch(high=df["high"], low=df["low"], close=df["close"], window= 14,smooth_window=3, fillna=False)
-            df["stoch_%D"] = df['Stoch_%K'].rolling(3).mean()
-        case "bol":
+            df["stoch_%D"] = df['stoch_%K'].rolling(3).mean()
+        case "boll":
             bol_band = ta.volatility.BollingerBands(close=df["close"], window=20, window_dev=2.25)
             df["lower_band"] = bol_band.bollinger_lband()
             df["higher_band"] = bol_band.bollinger_hband()
@@ -65,9 +66,9 @@ def create_plot(df, ind, pic_name):
 def plot_rsi(df, pic_name):
     df_plot = df.copy().iloc[-150:]
     s = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size': 6})
-    fig = mpf.figure(2, figsize=(20, 15), style=s)
-    ax1 = fig.add_subplot(2,1,1)
-    ax2 = fig.add_subplot(2,1,2, sharex=ax1)
+    fig1 = mpf.figure(1, figsize=(20, 15), style=s)
+    ax1 = fig1.add_subplot(2,1,1)
+    ax2 = fig1.add_subplot(2,1,2, sharex=ax1)
     ap0 = [
         mpf.make_addplot(df_plot["rsi"], color='purple', panel=0, title="rsi", ylabel='Points', ax=ax2),
         mpf.make_addplot(df_plot["rsima"], color='yellow', panel=0,ax=ax2)
@@ -83,9 +84,9 @@ def plot_rsi(df, pic_name):
 def plot_macd(df, pic_name):
     df_plot = df.copy().iloc[-150:]
     s = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size': 10})
-    fig = mpf.figure(2, figsize=(20, 15), style=s)
-    ax1 = fig.add_subplot(2,1,1)
-    ax2 = fig.add_subplot(2,1,2, sharex=ax1)
+    fig2 = mpf.figure(2, figsize=(20, 15), style=s)
+    ax1 = fig2.add_subplot(2,1,1)
+    ax2 = fig2.add_subplot(2,1,2, sharex=ax1)
     ap0 = [
         mpf.make_addplot(df_plot["macd"]/10, color='blue', panel=0, title="MACD", ylabel='Points', ax=ax2),
         mpf.make_addplot(df_plot["macd_signal"]/10, color='orange', panel=0, ax=ax2),
@@ -100,9 +101,9 @@ def plot_macd(df, pic_name):
 def plot_stoch(df, pic_name):
     df_plot = df.copy().iloc[-150:]
     s = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size': 10})
-    fig = mpf.figure(2, figsize=(20, 15), style=s)
-    ax1 = fig.add_subplot(2,1,1)
-    ax2 = fig.add_subplot(2,1,2, sharex=ax1)
+    fig3 = mpf.figure(3, figsize=(20, 15), style=s)
+    ax1 = fig3.add_subplot(2,1,1)
+    ax2 = fig3.add_subplot(2,1,2, sharex=ax1)
     ap0 = [
         mpf.make_addplot(df_plot["stoch_%K"], color='blue', panel=0, title="Stochastic", ylabel='Points', ax=ax2),
         mpf.make_addplot(df_plot["stoch_%D"], color='orange', panel=0, ax=ax2)
@@ -118,8 +119,8 @@ def plot_stoch(df, pic_name):
 def plot_boll(df, pic_name):
     df_plot = df.copy().iloc[-150:]
     s = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size': 10})
-    fig = mpf.figure(2, figsize=(20, 15), style=s)
-    ax1 = fig.add_subplot(2,1,1)
+    fig4 = mpf.figure(4, figsize=(20, 15), style=s)
+    ax1 = fig4.add_subplot(2,1,1)
     ap0 = [
         mpf.make_addplot(df_plot["lower_band"], color='blue', panel=0, title="Bollinger Bands", ax=ax1),
         mpf.make_addplot(df_plot["higher_band"], color='blue', panel=0, ax=ax1),
@@ -133,4 +134,9 @@ def plot_boll(df, pic_name):
 def plot_mm(df, pic_name):
     df_plot = df.copy().iloc[-150:]
     s = mpf.make_mpf_style(base_mpf_style='charles', rc={'font.size': 10})
-    mpf.plot(df_plot, mav=(9, 21), type='candle', style=s, figsize=(20, 10), savefig=pic_name)
+    fig5 = mpf.figure(5, figsize=(20, 15), style=s)
+    ax1 = fig5.add_subplot(2,1,1)
+    mpf.plot(df_plot, type='candle', ax=ax1, mav=(9, 21))
+    ax1.yaxis.set_label_position('left')
+    ax1.yaxis.tick_left()
+    plt.savefig(pic_name)
